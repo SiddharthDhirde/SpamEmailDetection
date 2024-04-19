b8ec79b04f71c1515432e2ffb3709399103ae045
Siddharth Dhirde <siddharthdhirde09@gmail.com>
2024-04-20 00:14:34 +0530

    change title to Spam Email Detection

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def transform_text(text):
 tfidf = pickle.load(open('vectorizer.pkl','rb'))
 model = pickle.load(open('model.pkl','rb'))
 
-st.title("Email Spam Classifier")
+st.title("Spam Email Detection")
 
 input_sms = st.text_area("Enter the message")
 
